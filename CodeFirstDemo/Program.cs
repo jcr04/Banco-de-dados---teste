efed25fa8fdb8ad82b98e437978f71175114fe8f
jcr04 <joaocarlosreboucas15@gmail.com>
2022-08-29 13:35:29 -0300

    projeto para estudo

---
+++ CodeFirstDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using CodeFirstDemo.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Criando e inicializando banco de dadosa partir do código C#...");
+            using (var db = new EscolaContext())
+            {
+                new EscolaInitializer().InitializeDatabase(db);
+            }
+            Console.WriteLine("Concluído!");
+            Console.ReadKey();
+        }
+    }
+}
